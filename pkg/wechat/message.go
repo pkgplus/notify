29dce32b354f8d3690680ed870a8e1e0acacc542
xuebing <xuebing1110@gmail.com>
2018-01-21 11:36:44 +0800

    modify page

--- pkg/wechat/message.go
+++ pkg/wechat/message.go
@@ -9,15 +9,18 @@ import (
 )
 
 func NoticeToTemplateMsg(n *wxmodels.Notice) (*wxmodels.TemplateMsg, error) {
-	if n.Page == "" {
-		n.Page = "/pages/index/index"
-	} else if strings.Index(n.Page, "/") < 0 {
-		n.Page = fmt.Sprintf("/pages/%s/%s", n.Page, n.Page)
-	}
+	// if n.Page == "" {
+	// 	n.Page = "/pages/index/index"
+	// } else if strings.Index(n.Page, "/") < 0 {
+	// 	n.Page = fmt.Sprintf("/pages/%s/%s", n.Page, n.Page)
+	// }
 
 	energy, err := storage.GlobalStore.PopEnergy(n.UserID)
 	if err != nil {
 		return nil, err
+	}
+	if n.Emphasis == "" {
+		n.Emphasis = "1"
 	}
 
 	tmsg := wxmodels.NewTemplateMsg(n.UserID, n.Template, energy, n.Values)
